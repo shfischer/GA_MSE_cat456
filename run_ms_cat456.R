6e45170a2e693e6805eb0b75ad8f81f174985871
Simon Fischer <32540427+shfischer@users.noreply.github.com>
2023-10-10 15:47:16 +0100

    renamed run_ms script

--- run_ms_cat456.R
+++ run_ms_cat456.R
@@ -317,224 +317,224 @@ if (isFALSE(ga_search)) {
 
 } else {
   
-  # ### ---------------------------------------------------------------------- ###
-  # ### prepare OM ####
-  # ### ---------------------------------------------------------------------- ###
-  # 
-  # if (isTRUE(nrow(hr_params) > 1)) 
-  #   stop("GA search only possible for one parameter (set)!")
-  # input <- do.call(input_mp, as.list(hr_params))
-  # 
-  # ### ------------------------------------------------------------------------ ###
-  # ### GA set-up ####
-  # ### ------------------------------------------------------------------------ ###
-  # 
-  # ### GA arguments
-  # ga_names <- c("idxB_lag", "idxB_range_3", "exp_b", "comp_b_multiplier",
-  #               "interval", "multiplier",
-  #               "upper_constraint", "lower_constraint")
-  # ga_default <- c(1, 1, 1, 1.4, 1, 1, Inf, 0)
-  # ga_lower <-   c(0, 1, 0, 0,   1, 0, 1,   0)
-  # ga_upper <-   c(1, 5, 2, 2,   5, 2, 5,   1)
-  # ga_suggestions <- rbind(#c(1, 1, 1, 1.4, 1, 1, Inf, 0), ### default
-  #                         #c(0, 1, 1, 1.4, 1, 1, Inf, 0), ### more recent data
-  #                         c(1, 1, 1, 1.4, 1, 0, Inf, 0), ### zero catch
-  #                         #c(1, 1, 1, 1.4, 2, 1, Inf, 0), ### biennial
-  #                         #c(0, 1, 1, 1.4, 2, 1, Inf, 0), ### biennial & more recent
-  #                         #c(1, 1, 1, 0,   1, 1, Inf, 0), ### without b
-  #                         #c(1, 1, 1, 1.4, 1, 1, 1.2, 0.8), ### +-20% cap
-  #                         #c(1, 1, 1, 1.4, 1, 1, 1.2, 0.7), ### +20% -30% cap
-  #                         expand.grid(0:1, 1, 1, c(0, 1, 1.4), 1:2, 1, 
-  #                                     c(1.2, Inf), c(0, 0.8))
-  #                        )
-  # ### turn of parameters not requested, i.e. limit to default value
-  # pos_default <- which(sapply(mget(ga_names, ifnotfound = FALSE), isFALSE))
-  # ga_lower[pos_default] <- ga_default[pos_default]
-  # ga_upper[pos_default] <- ga_default[pos_default]
-  # ### fix parameters?
-  # pos_fixed <- which(sapply(mget(ga_names, ifnotfound = FALSE), is.numeric))
-  # par_fixed <- names(pos_fixed)
-  # val_fixed <- as.vector(unlist(mget(ga_names, ifnotfound = FALSE)[pos_fixed]))
-  # ga_lower[pos_fixed] <- val_fixed
-  # ga_upper[pos_fixed] <- val_fixed
-  # ### remove not requested parameters from suggestions
-  # ga_suggestions[, pos_default] <- rep(ga_default[pos_default], 
-  #                                      each = nrow(ga_suggestions))
-  # ga_suggestions[, pos_fixed] <- rep(val_fixed, 
-  #                                    each = nrow(ga_suggestions))
-  # ga_suggestions <- unique(ga_suggestions)
-  # names(ga_suggestions) <- ga_names
-  # 
-  # ### multiplier only: run all possible values
-  # if (isTRUE(multiplier) &
-  #     !any(sapply(mget(setdiff(ga_names, "multiplier"), ifnotfound = FALSE),
-  #                   isTRUE))) {
-  #   m_vals <- seq(from = ga_lower[6], to = ga_upper[6], by = 0.01)
-  #   ga_suggestions[1, ] <- ga_lower
-  #   ga_suggestions <- ga_suggestions[rep(1, length(m_vals)), ]
-  #   ga_suggestions$multiplier <- m_vals
-  #   ### adapt GA dimensions
-  #   maxiter <- run <- 1
-  #   popSize <- length(m_vals)
-  #   run_all <- TRUE
-  # } else {
-  #   run_all <- FALSE
-  # }
-  # 
-  # 
-  # ### ---------------------------------------------------------------------- ###
-  # ### paths ####
-  # ### ---------------------------------------------------------------------- ###
-  # 
-  # ### output path
-  # ### set name depending on which GA parameters are used
-  # scn_pars <- ga_names[setdiff(seq_along(ga_names), pos_default)]
-  # ### add fixed parameters
-  # scn_pars[which(scn_pars %in% par_fixed)] <- paste0(
-  #   scn_pars[which(scn_pars %in% par_fixed)], val_fixed)
-  # scn_pars_c <- paste0(scn_pars, collapse = "-")
-  # #scenario <- "trial"
-  # path_out <- paste0("output/", MP, "/", n_iter, "_", n_yrs, "/", 
-  #                    scenario, "/", fhist, "/",
-  #                    paste0(names(input), collapse = "_"), "/")
-  # dir.create(path_out, recursive = TRUE)
-  # 
-  # ### objective function elements
-  # obj_fun <- c("SSB", "F", "C", "risk", "ICV", "ICES_PA", "ICES_PA2",
-  #              "ICES_MSYPA")
-  # obj_fun_use <- mget(x = paste0("obj_", obj_fun), 
-  #                     ifnotfound = FALSE)
-  # for (i in seq_along(obj_fun)) {
-  #   assign(x = paste0("obj_", obj_fun[i]), obj_fun_use[[i]])
-  # }
-  # obj_desc <- obj_fun[unlist(obj_fun_use)]
-  # obj_desc <- paste0("obj_", paste0(obj_desc, collapse = "_"), collapse = "")
-  # 
-  # ### store input data in temp file
-  # inp_file <- tempfile()
-  # saveRDS(object = input, file = inp_file, compress = FALSE)
-  # rm(input)
-  # gc()
-  # 
-  # ### ------------------------------------------------------------------------ ###
-  # ### check if previous solutions can be used as suggestions ####
-  # ### ------------------------------------------------------------------------ ###
-  # 
-  # ### years for summary statistics
-  # file_ext <- ifelse(stat_yrs == "all", "_res", 
-  #                    paste0("_res_", stat_yrs))
-  # ### suffix if different risk limit used
-  # file_ext <- ifelse(isTRUE(!identical(risk_threshold, 0.05) & 
-  #                             isTRUE(obj_ICES_MSYPA)), 
-  #                    paste0(file_ext, "_", risk_threshold), 
-  #                    file_ext)
-  # file_ext <- paste0(file_ext, ".rds")
-  # 
-  # if (isTRUE(add_suggestions)) {
-  #   ### find files
-  #   avail <- list.files(path_out, pattern = paste0("--", obj_desc, file_ext))
-  #   avail <- gsub(x = avail, pattern = paste0("--", obj_desc, file_ext),
-  #                 replacement = "")
-  #   avail <- strsplit(x = avail, split = "-")
-  #   ### need to have fewer parameters
-  #   avail <- avail[which(sapply(avail, length) < length(scn_pars))]
-  #   ### if some parameters fixed, remove suggestions without them
-  #   if (isTRUE(length(avail) > 0)) {
-  #     avail <- avail[which(sapply(avail, function(x) 
-  #       all(paste0(par_fixed, val_fixed) %in% x)))]
-  #     ### skip parameters not used
-  #     if (isTRUE(length(avail) > 0)) {
-  #       avail <- avail[which(sapply(avail, function(x) all(x %in% scn_pars)))]
-  #       if (isTRUE(length(avail) > 0)) {
-  #         ### load results
-  #         res_add <- lapply(avail, function(x) {
-  #           tmp <- readRDS(file = 
-  #             paste0(path_out, paste0(x, collapse = "-"), "--", obj_desc, 
-  #                    "_res", 
-  #                    ifelse(identical(stat_yrs, "all"), "", 
-  #                           paste0("_", stat_yrs)), 
-  #                    ".rds"))
-  #           tmp <- tmp@solution[1, ]
-  #           if (is.na(tmp[which("upper_constraint" == names(tmp))])) {
-  #             tmp[which("upper_constraint" == names(tmp))] <- Inf
-  #           }
-  #           return(tmp)
-  #         })
-  #         res_add <- do.call(rbind, res_add)
-  #         if (isTRUE(nrow(res_add) > 1)) {
-  #           res_add <- data.frame(res_add, stringsAsFactors = FALSE)
-  #         } else {
-  #           res_add <- data.frame(res_add, stringsAsFactors = FALSE)
-  #         }
-  #         cat("adding GA suggestions:\n")
-  #         print(res_add)
-  #         ### add to GA suggestions
-  #         ga_suggestions <- rbind(ga_suggestions, res_add)
-  #         ga_suggestions <- unique(ga_suggestions)
-  #       }
-  #     }
-  #   }
-  # }
-  # 
-  # ### ---------------------------------------------------------------------- ###
-  # ### run MSE with GA ####
-  # ### ---------------------------------------------------------------------- ###
-  # 
-  # ### set random seed for reproducibility
-  # registerDoRNG(123)
-  # set.seed(1)
-  # 
-  # ### run GA
-  # system.time({
-  #   res <- ga(type = "real-valued", fitness = mp_fitness, inp_file = inp_file,
-  #             obj_SSB = obj_SSB, obj_F = obj_F, obj_C = obj_C, 
-  #             obj_risk = obj_risk, obj_ICV = obj_ICV, obj_ICES_PA = obj_ICES_PA,
-  #             obj_ICES_PA2 = obj_ICES_PA2, obj_ICES_MSYPA = obj_ICES_MSYPA,
-  #             stat_yrs = stat_yrs, risk_threshold = risk_threshold,
-  #             path = path_out, check_file = check_file,
-  #             MP = MP,
-  #             suggestions = ga_suggestions, lower = ga_lower, upper = ga_upper,
-  #             names = ga_names,
-  #             maxiter = maxiter, popSize = popSize, run = run,
-  #             summarise_runs = TRUE,
-  #             postFitness = mp_postFitness,
-  #             keepBest = TRUE, parallel = cl1, seed = 1)
-  # })
-  # 
-  # ### save result
-  # saveRDS(object = res, file = paste0(path_out, scn_pars_c, 
-  #                                     "--", obj_desc, file_ext))
-  # 
-  # ### ---------------------------------------------------------------------- ###
-  # ### collate runs ####
-  # ### ---------------------------------------------------------------------- ###
-  # 
-  # if (isTRUE(collate)) {
-  #   files <- list.files(path = path_out, pattern = "[0-9]*[0-9].rds",
-  #                       full.names = FALSE)
-  #   files <- files[grep(x = files, pattern = "--", invert = TRUE)]
-  #   names(files) <- sapply(files, function(x) {
-  #     sub(x = x, pattern = ".rds", replacement = "", fixed = TRUE)
-  #   })
-  #   scns <- lapply(files, function(x) {
-  #     pars <- an(strsplit(sub(x = x, pattern = ".rds", replacement = "", fixed = TRUE), 
-  #                         split = "_")[[1]])
-  #     names(pars) <- ga_names
-  #     ### only keep scenarios where requested parameters are changed
-  #     if (!all(ga_default[pos_default] == pars[pos_default])) return(NULL)
-  #     if (!isTRUE(run_all)) {
-  #       if (!all(val_fixed == pars[pos_fixed])) return(NULL)
-  #     } 
-  #     stats <- readRDS(paste0(path_out, x))
-  #     list(pars = pars, stats = stats)
-  #   })
-  #   scns[sapply(scns, is.null)] <- NULL
-  #   #scns <- scns[order(sapply(scns, "[[", "obj"), decreasing = TRUE)]
-  #   saveRDS(scns, 
-  #           file = paste0(path_out, scn_pars_c, "--", obj_desc, "_runs",
-  #                         ifelse(identical(stat_yrs, "last10"), "_last10", ""), 
-  #                         ".rds"))
-  # }
+  ### ---------------------------------------------------------------------- ###
+  ### prepare OM ####
+  ### ---------------------------------------------------------------------- ###
+
+  if (isTRUE(nrow(hr_params) > 1))
+    stop("GA search only possible for one parameter (set)!")
+  input <- do.call(input_mp, as.list(hr_params))
+
+  ### ------------------------------------------------------------------------ ###
+  ### GA set-up ####
+  ### ------------------------------------------------------------------------ ###
+
+  ### GA arguments
+  ga_names <- c("idxB_lag", "idxB_range_3", "exp_b", "comp_b_multiplier",
+                "interval", "multiplier",
+                "upper_constraint", "lower_constraint")
+  ga_default <- c(1, 1, 1, 1.4, 1, 1, Inf, 0)
+  ga_lower <-   c(0, 1, 0, 0,   1, 0, 1,   0)
+  ga_upper <-   c(1, 5, 2, 2,   5, 2, 5,   1)
+  ga_suggestions <- rbind(#c(1, 1, 1, 1.4, 1, 1, Inf, 0), ### default
+                          #c(0, 1, 1, 1.4, 1, 1, Inf, 0), ### more recent data
+                          c(1, 1, 1, 1.4, 1, 0, Inf, 0), ### zero catch
+                          #c(1, 1, 1, 1.4, 2, 1, Inf, 0), ### biennial
+                          #c(0, 1, 1, 1.4, 2, 1, Inf, 0), ### biennial & more recent
+                          #c(1, 1, 1, 0,   1, 1, Inf, 0), ### without b
+                          #c(1, 1, 1, 1.4, 1, 1, 1.2, 0.8), ### +-20% cap
+                          #c(1, 1, 1, 1.4, 1, 1, 1.2, 0.7), ### +20% -30% cap
+                          expand.grid(0:1, 1, 1, c(0, 1, 1.4), 1:2, 1,
+                                      c(1.2, Inf), c(0, 0.8))
+                         )
+  ### turn of parameters not requested, i.e. limit to default value
+  pos_default <- which(sapply(mget(ga_names, ifnotfound = FALSE), isFALSE))
+  ga_lower[pos_default] <- ga_default[pos_default]
+  ga_upper[pos_default] <- ga_default[pos_default]
+  ### fix parameters?
+  pos_fixed <- which(sapply(mget(ga_names, ifnotfound = FALSE), is.numeric))
+  par_fixed <- names(pos_fixed)
+  val_fixed <- as.vector(unlist(mget(ga_names, ifnotfound = FALSE)[pos_fixed]))
+  ga_lower[pos_fixed] <- val_fixed
+  ga_upper[pos_fixed] <- val_fixed
+  ### remove not requested parameters from suggestions
+  ga_suggestions[, pos_default] <- rep(ga_default[pos_default],
+                                       each = nrow(ga_suggestions))
+  ga_suggestions[, pos_fixed] <- rep(val_fixed,
+                                     each = nrow(ga_suggestions))
+  ga_suggestions <- unique(ga_suggestions)
+  names(ga_suggestions) <- ga_names
+
+  ### multiplier only: run all possible values
+  if (isTRUE(multiplier) &
+      !any(sapply(mget(setdiff(ga_names, "multiplier"), ifnotfound = FALSE),
+                    isTRUE))) {
+    m_vals <- seq(from = ga_lower[6], to = ga_upper[6], by = 0.01)
+    ga_suggestions[1, ] <- ga_lower
+    ga_suggestions <- ga_suggestions[rep(1, length(m_vals)), ]
+    ga_suggestions$multiplier <- m_vals
+    ### adapt GA dimensions
+    maxiter <- run <- 1
+    popSize <- length(m_vals)
+    run_all <- TRUE
+  } else {
+    run_all <- FALSE
+  }
+
+
+  ### ---------------------------------------------------------------------- ###
+  ### paths ####
+  ### ---------------------------------------------------------------------- ###
+
+  ### output path
+  ### set name depending on which GA parameters are used
+  scn_pars <- ga_names[setdiff(seq_along(ga_names), pos_default)]
+  ### add fixed parameters
+  scn_pars[which(scn_pars %in% par_fixed)] <- paste0(
+    scn_pars[which(scn_pars %in% par_fixed)], val_fixed)
+  scn_pars_c <- paste0(scn_pars, collapse = "-")
+  #scenario <- "trial"
+  path_out <- paste0("output/", MP, "/", n_iter, "_", n_yrs, "/",
+                     scenario, "/", fhist, "/",
+                     paste0(names(input), collapse = "_"), "/")
+  dir.create(path_out, recursive = TRUE)
+
+  ### objective function elements
+  obj_fun <- c("SSB", "F", "C", "risk", "ICV", "ICES_PA", "ICES_PA2",
+               "ICES_MSYPA")
+  obj_fun_use <- mget(x = paste0("obj_", obj_fun),
+                      ifnotfound = FALSE)
+  for (i in seq_along(obj_fun)) {
+    assign(x = paste0("obj_", obj_fun[i]), obj_fun_use[[i]])
+  }
+  obj_desc <- obj_fun[unlist(obj_fun_use)]
+  obj_desc <- paste0("obj_", paste0(obj_desc, collapse = "_"), collapse = "")
+
+  ### store input data in temp file
+  inp_file <- tempfile()
+  saveRDS(object = input, file = inp_file, compress = FALSE)
+  rm(input)
+  gc()
+
+  ### ------------------------------------------------------------------------ ###
+  ### check if previous solutions can be used as suggestions ####
+  ### ------------------------------------------------------------------------ ###
+
+  ### years for summary statistics
+  file_ext <- ifelse(stat_yrs == "all", "_res",
+                     paste0("_res_", stat_yrs))
+  ### suffix if different risk limit used
+  file_ext <- ifelse(isTRUE(!identical(risk_threshold, 0.05) &
+                              isTRUE(obj_ICES_MSYPA)),
+                     paste0(file_ext, "_", risk_threshold),
+                     file_ext)
+  file_ext <- paste0(file_ext, ".rds")
+
+  if (isTRUE(add_suggestions)) {
+    ### find files
+    avail <- list.files(path_out, pattern = paste0("--", obj_desc, file_ext))
+    avail <- gsub(x = avail, pattern = paste0("--", obj_desc, file_ext),
+                  replacement = "")
+    avail <- strsplit(x = avail, split = "-")
+    ### need to have fewer parameters
+    avail <- avail[which(sapply(avail, length) < length(scn_pars))]
+    ### if some parameters fixed, remove suggestions without them
+    if (isTRUE(length(avail) > 0)) {
+      avail <- avail[which(sapply(avail, function(x)
+        all(paste0(par_fixed, val_fixed) %in% x)))]
+      ### skip parameters not used
+      if (isTRUE(length(avail) > 0)) {
+        avail <- avail[which(sapply(avail, function(x) all(x %in% scn_pars)))]
+        if (isTRUE(length(avail) > 0)) {
+          ### load results
+          res_add <- lapply(avail, function(x) {
+            tmp <- readRDS(file =
+              paste0(path_out, paste0(x, collapse = "-"), "--", obj_desc,
+                     "_res",
+                     ifelse(identical(stat_yrs, "all"), "",
+                            paste0("_", stat_yrs)),
+                     ".rds"))
+            tmp <- tmp@solution[1, ]
+            if (is.na(tmp[which("upper_constraint" == names(tmp))])) {
+              tmp[which("upper_constraint" == names(tmp))] <- Inf
+            }
+            return(tmp)
+          })
+          res_add <- do.call(rbind, res_add)
+          if (isTRUE(nrow(res_add) > 1)) {
+            res_add <- data.frame(res_add, stringsAsFactors = FALSE)
+          } else {
+            res_add <- data.frame(res_add, stringsAsFactors = FALSE)
+          }
+          cat("adding GA suggestions:\n")
+          print(res_add)
+          ### add to GA suggestions
+          ga_suggestions <- rbind(ga_suggestions, res_add)
+          ga_suggestions <- unique(ga_suggestions)
+        }
+      }
+    }
+  }
+
+  ### ---------------------------------------------------------------------- ###
+  ### run MSE with GA ####
+  ### ---------------------------------------------------------------------- ###
+
+  ### set random seed for reproducibility
+  registerDoRNG(123)
+  set.seed(1)
+
+  ### run GA
+  system.time({
+    res <- ga(type = "real-valued", fitness = mp_fitness, inp_file = inp_file,
+              obj_SSB = obj_SSB, obj_F = obj_F, obj_C = obj_C,
+              obj_risk = obj_risk, obj_ICV = obj_ICV, obj_ICES_PA = obj_ICES_PA,
+              obj_ICES_PA2 = obj_ICES_PA2, obj_ICES_MSYPA = obj_ICES_MSYPA,
+              stat_yrs = stat_yrs, risk_threshold = risk_threshold,
+              path = path_out, check_file = check_file,
+              MP = MP,
+              suggestions = ga_suggestions, lower = ga_lower, upper = ga_upper,
+              names = ga_names,
+              maxiter = maxiter, popSize = popSize, run = run,
+              summarise_runs = TRUE,
+              postFitness = mp_postFitness,
+              keepBest = TRUE, parallel = cl1, seed = 1)
+  })
+
+  ### save result
+  saveRDS(object = res, file = paste0(path_out, scn_pars_c,
+                                      "--", obj_desc, file_ext))
+
+  ### ---------------------------------------------------------------------- ###
+  ### collate runs ####
+  ### ---------------------------------------------------------------------- ###
+
+  if (isTRUE(collate)) {
+    files <- list.files(path = path_out, pattern = "[0-9]*[0-9].rds",
+                        full.names = FALSE)
+    files <- files[grep(x = files, pattern = "--", invert = TRUE)]
+    names(files) <- sapply(files, function(x) {
+      sub(x = x, pattern = ".rds", replacement = "", fixed = TRUE)
+    })
+    scns <- lapply(files, function(x) {
+      pars <- an(strsplit(sub(x = x, pattern = ".rds", replacement = "", fixed = TRUE),
+                          split = "_")[[1]])
+      names(pars) <- ga_names
+      ### only keep scenarios where requested parameters are changed
+      if (!all(ga_default[pos_default] == pars[pos_default])) return(NULL)
+      if (!isTRUE(run_all)) {
+        if (!all(val_fixed == pars[pos_fixed])) return(NULL)
+      }
+      stats <- readRDS(paste0(path_out, x))
+      list(pars = pars, stats = stats)
+    })
+    scns[sapply(scns, is.null)] <- NULL
+    #scns <- scns[order(sapply(scns, "[[", "obj"), decreasing = TRUE)]
+    saveRDS(scns,
+            file = paste0(path_out, scn_pars_c, "--", obj_desc, "_runs",
+                          ifelse(identical(stat_yrs, "last10"), "_last10", ""),
+                          ".rds"))
+  }
   
 }
   
